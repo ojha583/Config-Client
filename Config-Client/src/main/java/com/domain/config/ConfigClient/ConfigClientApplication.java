c03fddd55b2010548723c5ea5effe26eb58955d6
aojha <aojha@jcp.com>
2022-01-02 13:44:20 +0530

    Adding Config client changes

---
+++ Config-Client/src/main/java/com/domain/config/ConfigClient/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.domain.config.ConfigClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClientApplication.class, args);
+  }
+
+  @Value("${message:default config}")
+	private String config;
+
+  @GetMapping("/config")
+  public String getConfig() {
+		return  config;
+  }
+
+}
